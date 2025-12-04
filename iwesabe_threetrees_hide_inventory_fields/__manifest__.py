02a15015fdaa3fa0c4ced095e2b12d5455c8085a
Hiba Jmal <hibajamal97@email.com>
2025-12-04 09:10:02 +0300

    iwesabe_threetrees_hide_inventory_fields upgrade

---
+++ iwesabe_threetrees_hide_inventory_fields/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Iwesabe Threetrees Hide Stock Fields',
+    'category': 'Inventory/Purchase',
+    'version': '19.0.1.0.0',
+    'description': '',
+    'author': 'iwesabe',
+    'depends': ['iwesabe_threetrees_custom','iwesabe_branch_stock'],
+    'data': [
+        'views/stock_picking_views.xml',
+    ],
+    'application': True,
+    'installable': True,
+    'auto_install': False,
+    'license': 'OPL-1',
+}
