b45abab5899f0a372d5bff5ebdf6cd9b88e81f5d
Rayan Mohi <rayan.mohieldin@iatl-intellisoft.com>
2025-12-10 10:14:57 +0200

    update

--- iwesabe_branch_sale/model/sale_order_line.py
+++ iwesabe_branch_sale/model/sale_order_line.py
@@ -36,71 +36,75 @@ class SaleOrderLine(models.Model):
 
     branch_id = fields.Many2one('res.branch', default=_default_branch_id)
 
-    def _action_launch_stock_rule(self, previous_product_uom_qty=False):
-        """
-        Launch procurement group run method with required/custom fields genrated by a
-        sale order line. procurement group will launch '_run_pull', '_run_buy' or '_run_manufacture'
-        depending on the sale order line product rule.
-        """
-        precision = self.env['decimal.precision'].precision_get(
-            'Product Unit of Measure'
-        )
-        procurements = []
-        for line in self:
-            line = line.with_company(line.company_id)
-            if line.state != 'sale' or not line.product_id.type in ('consu','product'):
-                continue
-            qty = line._get_qty_procurement(previous_product_uom_qty)
-            if float_compare(qty, line.product_uom_qty, precision_digits=precision) == 0:
-                continue
-
-            group_id = line._get_procurement_group()
-            if not group_id:
-                group_id = self.env['procurement.group'].create(
-                    line._prepare_procurement_group_vals()
-                )
-                line.order_id.procurement_group_id = group_id
-            else:
-                # In case the procurement group is already created and the order was
-                # cancelled, we need to update certain values of the group.
-                updated_vals = {}
-                if group_id.partner_id != line.order_id.partner_shipping_id:
-                    updated_vals.update({
-                        'partner_id': line.order_id.partner_shipping_id.id
-                    })
-                if group_id.move_type != line.order_id.picking_policy:
-                    updated_vals.update({'move_type': line.order_id.picking_policy})
-                if updated_vals:
-                    group_id.write(updated_vals)
-
-            values = line._prepare_procurement_values(group_id=group_id)
-            # values.update({'branch_id': line.order_id.branch_id.id})
-
-            product_qty = line.product_uom_qty - qty
-
-            line_uom = line.product_uom
-            quant_uom = line.product_id.uom_id
-            product_qty, procurement_uom = line_uom._adjust_uom_quantities(
-                product_qty, quant_uom
-            )
-            procurements.append(self.env['procurement.group'].Procurement(
-                line.product_id, product_qty, procurement_uom,
-                line.order_id.partner_shipping_id.property_stock_customer,
-                line.name, line.order_id.name, line.order_id.company_id, values
-            ))
-        if procurements:
-            self.env['procurement.group'].run(procurements)
-
-        # This next block is currently needed only because the scheduler trigger is done by picking confirmation rather than stock.move confirmation
-        orders = self.mapped('order_id')
-        for order in orders:
-            pickings_to_confirm = order.picking_ids.filtered(
-                lambda p: p.state not in ['cancel', 'done']
-            )
-            if pickings_to_confirm:
-                # Trigger the Scheduler for Pickings
-                pickings_to_confirm.action_confirm()
-        return True
+    # def _action_launch_stock_rule(self, *, previous_product_uom_qty=False):
+    #     """
+    #     Launch procurement group run method with required/custom fields genrated by a
+    #     sale order line. procurement group will launch '_run_pull', '_run_buy' or '_run_manufacture'
+    #     depending on the sale order line product rule.
+    #     """
+    #     precision = self.env['decimal.precision'].precision_get(
+    #         'Product Unit of Measure'
+    #     )
+    #     procurements = []
+    #     for line in self:
+    #         line = line.with_company(line.company_id)
+    #         if line.state != 'sale' or not line.product_id.type in ('consu','product'):
+    #             continue
+    #         qty = line._get_qty_procurement(previous_product_uom_qty)
+    #         if float_compare(qty, line.product_uom_qty, precision_digits=precision) == 0:
+    #             continue
+    #
+    #         references = line.order_id.stock_reference_ids
+    #         if not references:
+    #             self.env['stock.reference'].create(line._prepare_reference_vals())
+    #
+    #         group_id = line._get_procurement_group()
+    #         if not group_id:
+    #             group_id = self.env['stock.rule'].create(
+    #                 line._prepare_procurement_group_vals()
+    #             )
+    #             line.order_id.procurement_group_id = group_id
+    #         else:
+    #             # In case the procurement group is already created and the order was
+    #             # cancelled, we need to update certain values of the group.
+    #             updated_vals = {}
+    #             if group_id.partner_id != line.order_id.partner_shipping_id:
+    #                 updated_vals.update({
+    #                     'partner_id': line.order_id.partner_shipping_id.id
+    #                 })
+    #             if group_id.move_type != line.order_id.picking_policy:
+    #                 updated_vals.update({'move_type': line.order_id.picking_policy})
+    #             if updated_vals:
+    #                 group_id.write(updated_vals)
+    #
+    #         values = line._prepare_procurement_values(group_id=group_id)
+    #         # values.update({'branch_id': line.order_id.branch_id.id})
+    #
+    #         product_qty = line.product_uom_qty - qty
+    #
+    #         line_uom = line.product_uom
+    #         quant_uom = line.product_id.uom_id
+    #         product_qty, procurement_uom = line_uom._adjust_uom_quantities(
+    #             product_qty, quant_uom
+    #         )
+    #         procurements.append(self.env['stock.rule'].Procurement(
+    #             line.product_id, product_qty, procurement_uom,
+    #             line.order_id.partner_shipping_id.property_stock_customer,
+    #             line.name, line.order_id.name, line.order_id.company_id, values
+    #         ))
+    #     if procurements:
+    #         self.env['stock.rule'].run(procurements)
+    #
+    #     # This next block is currently needed only because the scheduler trigger is done by picking confirmation rather than stock.move confirmation
+    #     orders = self.mapped('order_id')
+    #     for order in orders:
+    #         pickings_to_confirm = order.picking_ids.filtered(
+    #             lambda p: p.state not in ['cancel', 'done']
+    #         )
+    #         if pickings_to_confirm:
+    #             # Trigger the Scheduler for Pickings
+    #             pickings_to_confirm.action_confirm()
+    #     return True
 
     def _prepare_invoice_line(self, **optional_values):
         res = super(SaleOrderLine, self)._prepare_invoice_line(**optional_values)
@@ -126,8 +130,8 @@ class SaleOrderLine(models.Model):
 
 
 
-class ProcurementGroup(models.Model):
-    _inherit = 'procurement.group'
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
 
     def _default_branch_id(self):
         branch_id = self.env['res.users'].browse(self._uid).branch_id.id
